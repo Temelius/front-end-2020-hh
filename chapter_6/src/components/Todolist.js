e4aa18765cf0f26ed19af366ecce4f3246af21c6
Temelius <teme989@gmail.com>
2020-09-27 23:15:30 +0300

    Exercise 13 Todo with delete

--- chapter_6/src/components/Todolist.js
+++ chapter_6/src/components/Todolist.js
@@ -8,13 +8,19 @@ function Todolist () {
   })
   const [todos, setTodos] = useState([])
 
-  const inputChanged =  (event) => {
+  const inputChanged = (event) => {
     setTodo({...todo, [event.target.name]: event.target.value})
   }
 
   const addTodo = () => {
     setTodos([...todos, todo])
     setTodo({desc:'',date:''})
+  }
+
+  const deleteTodo = (index) => {
+    const newList = todos.filter((todo, i) => i !== index)
+
+    setTodos(newList)
   }
 
   return(
@@ -30,9 +36,16 @@ function Todolist () {
           <tr>
             <th>Description</th>
             <th>Date</th>
+            <th></th>
           </tr>
           {
-            todos.map((todo, index) => <tr key={index}><td>{todo.desc}</td><td>{todo.date}</td></tr>)
+            todos.map((todo, index) =>
+              <tr key={index}>
+                <td>{todo.desc}</td>
+                <td>{todo.date}</td>
+                <td><button onClick={() => deleteTodo(index)}>Delete</button></td>
+              </tr>
+            )
           }
         </tbody>
       </table>
