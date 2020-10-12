59d21a8995164edf4743a342e2d36931219ee230
Temelius <teme989@gmail.com>
2020-10-12 05:04:52 +0300

    Change table to be rendered with AgGrid

--- chapter_6/src/components/Todolist.js
+++ chapter_6/src/components/Todolist.js
@@ -1,5 +1,12 @@
-import React, { useState } from 'react'
-import TodoTable from './TodoTable.js'
+import React, { useState, useRef } from 'react'
+
+import { AgGridReact } from 'ag-grid-react'
+
+import 'ag-grid-community/dist/styles/ag-grid.css'
+import 'ag-grid-community/dist/styles/ag-theme-material.css'
+
+// import TodoTable from './TodoTable.js'
+
 
 function Todolist () {
 
@@ -9,6 +16,7 @@ function Todolist () {
     priority: ''
   })
   const [todos, setTodos] = useState([])
+
 
   const inputChanged = (event) => {
     setTodo({...todo, [event.target.name]: event.target.value})
@@ -20,10 +28,29 @@ function Todolist () {
   }
 
   const deleteTodo = (index) => {
-    const newList = todos.filter((todo, i) => i !== index)
+    // This code works with the AgGridReact module
+    if (gridRef.current.getSelectedNodes().length > 0) {
+      setTodos(todos.filter((todo, index) => index !== gridRef.current.getSelectedNodes()[0].childIndex))
+    } else {
+      alert('Select row first')
+    }
 
-    setTodos(newList)
+    // If we use the external TodoTable.js component to render the table,
+    //  use this code below
+    // setTodos(todos.filter((todo, i) => i !== index))
+    // Remember to add this to render-> <TodoTable delete={deleteTodo} todos={todos}/>
+
   }
+
+  const gridRef = useRef();
+
+  const columns = [
+    { headerName: "Description", field: "desc", sortable: true, filter: true, floatingFilter: true },
+    { headerName: "Date", field: "date", sortable: true, filter: true, floatingFilter: true },
+    { headerName: "Priority", field: "priority", sortable: true, filter: true, floatingFilter: true,
+      cellStyle: params => params.value === "High" ? {color: 'red'} : {color: 'black'}
+    }
+  ]
 
   return(
     <div>
@@ -35,7 +62,18 @@ function Todolist () {
       <label>Priority:</label>
       <input type="text" name="priority" value={todo.priority} onChange={inputChanged} />
       <button onClick={addTodo}>Add</button>
-      <TodoTable delete={deleteTodo} todos={todos}/>
+      <button onClick={deleteTodo}>Delete</button>
+
+      <div className="ag-theme-material" style={{height: '500px'}}>
+        <AgGridReact
+          ref={gridRef}
+          onGridReady={ params => gridRef.current = params.api}
+          rowSelection="single"
+          columnDefs={columns}
+          rowData={todos}
+          animateRows="true">
+        </AgGridReact>
+      </div>
     </div>
   )
 }
