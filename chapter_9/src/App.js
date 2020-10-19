7f6bffee9a94615bb9647f062505e593d9ae6c54
Temelius <teme989@gmail.com>
2020-10-19 05:01:34 +0300

    Add chapter 9 task

--- chapter_9/src/App.js
+++ chapter_9/src/App.js
@@ -2,23 +2,45 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { gql } from 'apollo-boost'
+import { useQuery } from '@apollo/react-hooks'
+
 function App() {
+  const countryQuery = gql`
+    {
+      countries {
+        code,
+        name,
+        continent { name }
+      }
+    }
+  `;
+
+  const { loading, error, data } = useQuery(countryQuery)
+
+  if (loading) return "Loading..."
+  if (error) return `Error ${error.message}`
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <tbody>
+          <tr>
+            <th><b>Code</b></th>
+            <th><b>Country</b></th>
+            <th><b>Continent</b></th>
+          </tr>
+          {
+            data.countries.map((country, index) =>
+              <tr key={index}>
+                <td>{country.code}</td>
+                <td>{country.name}</td>
+                <td>{country.continent.name}</td>
+              </tr>
+            )
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
