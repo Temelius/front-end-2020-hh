5cd65d93dd66718989930425ee4a0029567b5b69
Temelius <teme989@gmail.com>
2020-12-11 04:57:04 +0200

    Add carshop exercise

---
+++ chapter_13_carshop/src/App.js
@@ -0,0 +1,12 @@
+import Carlist from './components/Carlist';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Carlist />
+    </div>
+  );
+}
+
+export default App;
